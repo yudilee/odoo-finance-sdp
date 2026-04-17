99767afbc9424f1f8c9e895cbaf76d13d369702f
yudilee <lee140685@gmail.com>
2026-04-17 15:38:13 +0700

    chore: remove temporary test files

--- test_export.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-$service = app(\App\Services\OdooService::class);
-$moves = $service->execute('account.move', 'search', [[['name', '=', 'INVRS/2026/04058']]]);
-$exportFields = ['name', 'invoice_line_ids/name', 'invoice_line_ids/start_rental_period', 'invoice_line_ids/end_rental_period'];
-$result = $service->execute('account.move', 'export_data', [$moves, $exportFields]);
-print_r($result);
